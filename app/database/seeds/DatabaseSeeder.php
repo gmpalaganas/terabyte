f138951b641ee84a699d8556b1b0dea0314c6dce
Genesis Palaganas <itetsky123@gmail.com>
2015-06-17 13:37:10 +0800

    Added Seeds for the database

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -7,11 +7,55 @@ class DatabaseSeeder extends Seeder {
 	 *
 	 * @return void
 	 */
-	public function run()
-	{
-		Eloquent::unguard();
+    
+    public function run(){
+        
+       $this->call('CategoryTableSeeder'); 
+       $this->call('ItemTableSeeder');
+    }    
 
-		// $this->call('UserTableSeeder');
+}
+
+class CategoryTableSeeder extends Seeder {
+    
+    public function run(){
+        
+        DB::table('categories')->delete();        
+
+        $categories = array('Mouse','Keyboard','Monitor');
+
+        foreach($categories as $category){
+            DB::table('categories')->insert(array('name'=>$category));
+        }
+        
 	}
+
+}
+
+class ItemTableSeeder extends Seeder {
+    
+    public function run(){
+
+        DB::table('items')->delete();
+
+        $items = [ 
+            'Razer Naga' => ['Mouse', 5000 ], 
+            'SteelSeries Sensei' => ['Mouse', 4500],
+            'Razer Anansi' => ['Keyboard', 6000],
+            'SteelSeries Apex' => ['Keyboard', 7000],
+            'ASUS ROG Swift' => ['Monitor', 10000],
+            'Acer XB280HK' => ['Monitor', 120000],
+        ]; 
+
+        foreach( $items as $item => $details){
+            $category = Category::where('name','=',$details[0])->select('id')->first();
+            DB::table('items')->insert(array(
+               'name' => $item,
+               'price' => $details[1],
+               'category_id' => $category->id
+            )); 
+        }
+
+    }
 
 }
