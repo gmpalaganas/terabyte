fba18cdc4ae8804e672ca0e597325b1fa7958a23
Genesis Palaganas <itetsky123@gmail.com>
2015-06-17 12:22:13 +0800

    Created the scehema for the project

---
+++ app/database/migrations/2015_06_17_041334_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('items', function($table){
+            $table->increments('id');
+            $table->string('name');
+            $table->float('price')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
