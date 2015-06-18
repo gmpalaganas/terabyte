e2701fa7d1d0aa74e1cb87a5f2050f0c575f10e7
YOUR NAME <karlos.raya@gmail.com>
2015-06-18 15:31:16 +0800

    Other pages

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Delete Item</title>
+    <title>About</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/font-awesome.min.css" rel="stylesheet">
     <link href="assets/css/prettyPhoto.css" rel="stylesheet">
@@ -37,7 +37,7 @@
 					<ul class="nav navbar-nav">
 						<h3>TERABYTE</h3>
 						<li class=><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
+                        <li class="active"><a href="#">About<span class="sr-only">(current)</span></a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">View By Category<span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
@@ -56,13 +56,25 @@
         </div>
     </header><!--/header-->
     <div align="center">
-        <br><br><br>
-        <h1>Delete Item</h1>
-        Item Name:<br>
-        <input type="text" name="itemname">
-        <br><br>
-        <input type="submit" value="Submit">
-        <br><br>
+        <div style=display:inline-block >
+            <br><br> <br>
+            <h1>Cozee Internship Team</h1>
+            <br>
+            <a href="https://www.facebook.com/laloo179/about"><img src="https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/10882117_10153170240419264_7079083114914426711_n.jpg?oh=fd08db47fda58c3b46764dff19ac0dab&oe=55E9A36F&__gda__=1442159674_c00d3a31354ffeba84371b15d9fce63f" alt="Karlos" style="width:200px;height:300px;"></a></h3>
+            
+            [Insert Description Here]
+
+             <a href="https://www.facebook.com/jolina.gaspar?fref=ts"> <img src="https://scontent-sea1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/11058389_1025251287489524_8563595427559725312_n.jpg?oh=b2b4daa01d5d827c47f9f32646fa1b8a&oe=55F8FD3C" alt="Jolina" style="width:200px;height:300px;"></a>
+           
+            [Insert Description Here]
+
+            <a href="https://www.facebook.com/palaganas.gi?fref=ts"><img src="https://scontent-sea1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/11011772_10205430429672943_2878981633824172866_n.jpg?oh=ba5ef2a2e2b862eec88d3e186e4e53a9&oe=55EF04E7" alt="Genesis" style="width:200px;height:300px;"></a>
+            
+            [Insert Description Here]
+            <br>
+            <br>
+
+        </div>   
     </div> 
 	
     <footer id="footer" class="midnight-blue">
