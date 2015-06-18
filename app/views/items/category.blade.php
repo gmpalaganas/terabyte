01d7f862ab816521a081c299e73a82928b8f7138
Genesis Palaganas <itetsky123@gmail.com>
2015-06-18 20:24:49 +0800

    Added file needed for AJAX in view by Category

---
+++ app/views/items/category.blade.php
@@ -0,0 +1,14 @@
+@foreach($items as $item)
+<div class="col-xs-4">
+    <div class="portfolio-item">
+        <div class="item-inner">
+            <img class="img-responsive" src="assets/images/items/1.jpg" alt="">
+            <h5>
+                {{ $item->name }}
+            </h5>
+            <h6> {{ $item->category }} </h6>
+            <h6> Php {{$item->price}} </h6>
+        </div>
+    </div>
+</div>                            
+@endforeach
